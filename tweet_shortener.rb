69ebc80c155e035ff9c950b5f995fb5eaa704932
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-03-10 21:40:21 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -35,7 +35,7 @@ end
 
 def selective_tweet_shortener(tweet)
       if tweet.length > 140
-      word_substituter(phrase)
+      word_substituter(tweet)
     elsif tweet.length <= 140
       tweet
     end
